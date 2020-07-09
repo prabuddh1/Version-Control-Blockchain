3196b46fa77ae4852ada1e2b0f47e4eb34508cd4
Prabuddh Kumar Ashok <48401909+prabuddh1@users.noreply.github.com>
2020-07-09 23:48:44 +0530

    Update Version.sol

--- contracts/Version.sol
+++ contracts/Version.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.4.25;
 
-//factory contract section
 
-contract Factory{
+//Create factory
+
+
+contract CreateVideo{
     address[] public deployedContractList;
     
-    function Upload(string location) external{
-        address newContract = new Version(location,msg.sender);//instantiating new contract syntax
+    constructor() public{
+        address newContract = new Upload(msg.sender);//instantiating new contract syntax
+        deployedContractList.push(newContract);
+    
+    }
+    
+    
+    function getDeployedContracts() external view returns(address[]){
+        return deployedContractList;
+    }
+
+    
+    
+}
+
+// UPLOAD factory contract section
+
+contract Upload{
+    address[] public deployedContractList;
+    
+    constructor(address sender) public{
+        address newContract = new Version(sender);//instantiating new contract syntax
         deployedContractList.push(newContract);
     
     }
@@ -26,30 +48,50 @@ contract Version{
     address public owner;
     string[] private versions;
     uint version_count;
-    uint endTime;
-    enum Stage{ CanFork,CannotFork}
-    Stage public stage=Stage.CanFork;
     
-    struct Editor{ 
+    
+    
+    struct Editor{
         address editor_address;
         bool access;
         bool editing_not_done;
-        uint version_name;
+        
+        uint editCount;
     }
     
+  //  event Sunidhi(address edit,uint value);
     
     
     
-    
-    address[] public editor_list;
+    address[] private editor_list;
     
     mapping(address => Editor) editor;
     
     
     
-    constructor(string location,address sender) public{
+    constructor(address sender) public{
         
         owner=sender;
+       
+    }
+    
+   
+    
+    modifier access(address sender){
+        require(editor[sender].access==true);
+        _;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender==owner);
+        _;
+    }
+       
+    
+       
+   function UploadVideo(string location) external onlyOwner{
+       
+        
         versions.push(location);
         version_count++;
        
@@ -57,60 +99,34 @@ contract Version{
         editor[owner].editor_address=owner;
         editor[owner].access=true;
         editor[owner].editing_not_done=false;
-        editor[owner].version_name=1;
-        
-    }
+       
+        editor[owner].editCount++;
+   }
     
-    modifier forkingTime(){
-        require(stage==Stage.CanFork);
-        _;
-    }
-    
-    
-    modifier waitingTime(){
-        require(stage==Stage.CannotFork);
-        _;
-        
-    }
-    
-   function fork() external forkingTime{
+   function fork() external{
        //access modifiers with timeframe:
        
         editor[msg.sender].editor_address=msg.sender;
         editor[msg.sender].access=true;
         editor[msg.sender].editing_not_done=true;
         
-        stage=Stage.CannotFork;
-      //  endTime=now+20 seconds;
-        //resetTimeFrame(msg.sender);
+
+     
         
     }
-    
-    /*
-    function resetTimeFrame(address forker) private {
-        
-        if(now>endTime)
-        {
-            stage=Stage.CanFork;
-            editor[forker].access=false;
-            
-        }
-        //this function is invoked when a person forks the video....once the time limit is exceeded,the video can be forked again regardless
-       // of whether he or she has edited it
-    }
-   */
-   function Update(string location) external waitingTime{
+
+   function MergeRequest(string location) external access(msg.sender){
         
         
         
         versions.push(location);
         version_count++;
+        editor[msg.sender].editCount++;
         editor[msg.sender].editing_not_done=false;
-        editor[msg.sender].version_name=version_count;
         editor_list.push(msg.sender);
         
         //changing time back to timeframe when it can be forked
-        stage=Stage.CanFork;
+        
     }
    
    function getEditorList() external view returns(address[]){
@@ -121,7 +137,7 @@ contract Version{
    
    function BasicINcentiveMOdel(address candidate) external view returns(uint){
        
-       return(editor[candidate].version_name);
+       return(editor[candidate].editCount);
        
    }
     
