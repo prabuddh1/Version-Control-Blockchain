9a8d1ce2426f9ff3d9e365ed8e73f459d4ba23ff
Prabuddh Kumar Ashok <48401909+prabuddh1@users.noreply.github.com>
2020-07-15 16:51:21 +0530

    Updated Version.sol

--- contracts/Version.sol
+++ contracts/Version.sol
@@ -6,92 +6,100 @@ pragma solidity ^0.4.25;
 
 contract CreateVideo{
     address[] public deployedContractList;
-    
+   
     constructor() public{
-        address newContract = new Upload(msg.sender);//instantiating new contract syntax
+        address newContract = new Upload();//instantiating new contract syntax
         deployedContractList.push(newContract);
-    
+   
     }
-    
-    
+   
+   
     function getDeployedContracts() external view returns(address[]){
         return deployedContractList;
     }
 
-    
-    
+   
+   
 }
 
 // UPLOAD factory contract section
 
 contract Upload{
-    address[] public deployedContractList;
-    
-    constructor(address sender) public{
-        address newContract = new Version(sender);//instantiating new contract syntax
+    address[] private deployedContractList;
+   
+    constructor() public{
+        address newContract = new Version(msg.sender);//instantiating new contract syntax
         deployedContractList.push(newContract);
-    
+   
     }
-    
-    
+   
+    function CreateNewMasterBranch() external{
+        address newContract = new Version(msg.sender);//instantiating new contract syntax
+        deployedContractList.push(newContract);
+       
+    }
+   
+   
     function getDeployedContracts() external view returns(address[]){
         return deployedContractList;
     }
 
-    
-    
+   
+   
 }
 
 //main contract section
 contract Version{
-    
+   
     address public owner;
     string[] private versions;
     uint version_count;
-    
-    
-    
+   
+   
+   
     struct Editor{
         address editor_address;
         bool access;
         bool editing_not_done;
-        
+       
         uint editCount;
     }
-    
+   // struct Request
+   
+   
   //  event Sunidhi(address edit,uint value);
-    
-    
-    
+   
+   
+   
     address[] private editor_list;
-    
+   
     mapping(address => Editor) editor;
-    
-    
-    
+   
+   
+   
     constructor(address sender) public{
-        
+       
         owner=sender;
        
     }
-    
    
-    
+   
+   
     modifier access(address sender){
         require(editor[sender].access==true);
         _;
     }
-    
+   
     modifier onlyOwner(){
         require(msg.sender==owner);
         _;
     }
        
-    
+   
        
    function UploadVideo(string location) external onlyOwner{
        
-        
+       
         versions.push(location);
         version_count++;
        
@@ -102,31 +110,31 @@ contract Version{
        
         editor[owner].editCount++;
    }
-    
+   
    function fork() external{
        //access modifiers with timeframe:
        
         editor[msg.sender].editor_address=msg.sender;
         editor[msg.sender].access=true;
         editor[msg.sender].editing_not_done=true;
-        
+       
 
      
-        
+       
     }
 
    function MergeRequest(string location) external access(msg.sender){
-        
-        
-        
+       
+       
+       
         versions.push(location);
         version_count++;
         editor[msg.sender].editCount++;
         editor[msg.sender].editing_not_done=false;
         editor_list.push(msg.sender);
-        
+       
         //changing time back to timeframe when it can be forked
-        
+       
     }
    
    function getEditorList() external view returns(address[]){
@@ -140,13 +148,13 @@ contract Version{
        return(editor[candidate].editCount);
        
    }
-    
+   
    function getLatestVersion() external view returns(string){
        
        return(versions[version_count-1]);
    }
-    
-    
-    
-    
+   
+   
+   
+   
 }
